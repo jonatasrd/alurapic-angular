7786132b87b391b103f78091a8f3362dcbc4aa26
Jonatas Ramos <jonatasrd@gmail.com>
2018-06-13 17:04:35 -0300

    Adicionado mais módulos

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,11 +1,18 @@
+import 'rxjs/add/operator/map';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FotoModule } from './foto/foto.module';
+import { HttpModule} from '@angular/http';
+import { PainelModule } from './painel/painel.module';
+
+import { CadastroComponent }   from './cadastro/cadastro.component'; // importou
+import { ListagemComponent }   from './listagem/listagem.component'; // importou
 
 @NgModule({
-    imports:      [ BrowserModule, FotoModule ],
-    declarations: [ AppComponent ],
+    imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule],
+    declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
     bootstrap:    [ AppComponent ]
   })
 
---
+++ client/app/painel/painel.module.ts
@@ -0,0 +1,10 @@
+// alurapic/client/app/painel/painel.module.ts
+
+import { NgModule } from '@angular/core';
+import { painelComponent } from './painel.component';
+
+@NgModule({
+  declarations: [ painelComponent ],
+  exports: [painelComponent]
+})
+export class PainelModule { }
